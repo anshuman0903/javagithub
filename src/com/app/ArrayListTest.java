07050ecde7bf4ed9b375b90e5eacef263fe67dcb
abc <abc@example.com>
2020-01-11 18:05:35 +0530

    i have written code to fetch index 1 code.

---
+++ src/com/app/ArrayListTest.java
@@ -0,0 +1,20 @@
+package com.app;
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList list=new ArrayList();
+		list.add(12);
+		list.add(13);
+		list.add(14);
+		list.add(15);
+		list.add(16);
+		System.out.println(list);
+		System.out.println(list.get(1));
+
+	}
+
+}
